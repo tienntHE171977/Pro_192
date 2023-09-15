26636f46aa2e5c2d8e30dd1f46c5a3d8dc7f7f6e
tienntHE171977 <144331133+tienntHE171977@users.noreply.github.com>
2023-09-15 21:45:47 +0700

    Lap1

---
+++ workshop1/src/workshop1/Workshop1.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author FPT
+ */
+public class workshop1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter numbers of rows: ");
+        int hang = sc.nextInt();
+        System.out.print("Enter nubers of columns: ");
+        int cot = sc.nextInt();
+        int[][] matrix = new int[hang][cot];
+        System.out.println("Enter the matrix:");
+        for(int i=0 ;i<hang;i++){
+            for(int j=0;j<cot;j++){
+                System.out.print("m[" + i + "][" + j + "]=");
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix inputted:");
+        for(int i=0; i<hang;i++){
+            for(int j=0;j<cot;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+        int sum =0;
+        for(int i=0;i<hang;i++){
+            for(int j=0;j<cot;j++){
+                sum += matrix[i][j];
+            }
+        }
+        double trungbinh = (double)sum / (hang * cot);
+        System.out.println("Sum: "+ sum);
+        System.out.println("Average: "+ trungbinh);
+    }
+    
+}
